4175d30b19bdbf6978263db6d57ab5a1757d0b07
niishaparashar <nishaparashar1712@gmail.com>
2026-02-08 11:51:58 +0530

    Fix API base URL

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,6 +1,7 @@
 console.log("api.js loaded");
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://vecron.onrender.com";
+
 
 function getToken() {
   return localStorage.getItem("access_token");
